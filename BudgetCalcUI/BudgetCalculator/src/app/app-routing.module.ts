f14f78961dbaa49ab36e0d938b62bf1b67e9b89d
loganMaleady <loganmaleady16@gmail.com>
2024-05-08 23:50:46 -0400

    Set up routing and added tabs

--- BudgetCalcUI/BudgetCalculator/src/app/app-routing.module.ts
+++ BudgetCalcUI/BudgetCalculator/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { CalculateComponent } from './calculate/calculate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "dash", component: DashBoardComponent},
+  { path: "calculate", component: CalculateComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BudgetCalcUI/BudgetCalculator/src/app/app.module.ts
+++ BudgetCalcUI/BudgetCalculator/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatTab } from '@angular/material/tabs';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    CalculateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatCardModule,
+    MatTableModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
